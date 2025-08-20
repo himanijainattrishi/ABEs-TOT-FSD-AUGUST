426aa7f1935674171e2aff7af399ea5b9f35bd16
himanijainattrishi <himani.jain@abes.ac.in>
2025-08-20 10:52:17 +0530

    task done

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,15 @@
 console.log("hi javascript")
+var user={
+    name:'abc',
+    rollno:23,
+// object
+abc()
+{
+console.log(this.name)
+console.log(this.rollno)
+}}
+
+let admin=user
+user=null
+admin.abc()
+console.log(admin.rollno)
