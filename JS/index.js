dc86095cd371a6a0f9dbca23d375b0a78c4b12ee
himanijainattrishi <himani.jain@abes.ac.in>
2025-08-20 11:13:54 +0530

    task done

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,39 @@
 console.log("hi javascript")
-var user={
-    name:'abc',
-    rollno:23,
-// object
-abc()
-{
-console.log(this.name)
-console.log(this.rollno)
-}}
+// var user={
+//     name:'abc',
+//     rollno:23,
+// // object
+// abc()
+// {
+// console.log(this.name)
+// console.log(this.rollno)
+// }}
 
-let admin=user
-user=null
-admin.abc()
-console.log(admin.rollno)
+// let admin=user
+// user=null
+// admin.abc()
+// console.log(admin.rollno)
+
+// let user={
+//     name:'abc'
+// }
+// let admin={
+//     name:'admin'
+// }
+// function sayHi()
+// {
+//    console.log(this.name)
+// }
+// admin.abc=sayHi
+// admin.abc()
+
+let group={
+    dep:'MCA',
+    name:['abc','xyz','pp'],
+
+    sayHi()
+    {
+        console.log(group.name[0]+group.dep)
+    }
+};
+group.sayHi()
