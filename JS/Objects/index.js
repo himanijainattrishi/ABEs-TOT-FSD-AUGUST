3cd5affbb89d72b19618867b6931169b04c7ffd8
himanijainattrishi <himani.jain@abes.ac.in>
2025-08-21 11:09:32 +0530

    task done

--- JS/Objects/index.js
+++ JS/Objects/index.js
@@ -1,7 +1,26 @@
-const user={
-    name:'xyz',
-    age:20,
-    phone:[1234,5678,4567]
+// const user={
+//     name:'xyz',
+//     age:20,
+//     phone:[1234,5678,4567]
+// }
+// console.log(user.phone)
+// console.log(user.name,user.phone[1])
+
+const car={
+name:['xuv','exter'],
+    engine:[
+        'abc','xyz'
+    ],
+
+  
+    type:['type1','type2'],
+
+    
+    power:['power1','power2']
+
+    
+
 }
-console.log(user.phone)
-console.log(user.name,user.phone[1])
+for(i in car.engine){
+    console.log(car.engine[1])
+}
