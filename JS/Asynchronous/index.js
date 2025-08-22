d73f3bba9d0bf36fce680167e3c76344d73d4790
himanijainattrishi <himani.jain@abes.ac.in>
2025-08-22 10:32:12 +0530

    task done

---
+++ JS/Asynchronous/index.js
@@ -0,0 +1,33 @@
+// how we can achive async function 
+//     1) call back
+
+// }
+
+function register()
+{
+console.log("register user")
+}
+function sendEmail()
+{console.log("email send")
+
+}
+function login()
+{
+console.log("login here")
+}
+function getData()
+{
+console.log("getdata")
+}
+function display()
+{
+console.log("display data")
+}
+
+register();
+sendEmail()
+login()
+getData()
+display()
+console.log("call other application");
+
